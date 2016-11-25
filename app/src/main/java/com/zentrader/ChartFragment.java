51dcccc46c3c4ebd5630737131842e313df74b4f
Hasham <hasham.rashid@outlook.com>
2016-11-25 21:46:22 +0000

    added chart

--- app/src/main/java/com/zentrader/ChartFragment.java
+++ app/src/main/java/com/zentrader/ChartFragment.java
@@ -14,6 +14,13 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class ChartFragment extends Fragment {
@@ -30,8 +37,23 @@ public class ChartFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.chart_layout, container, false);
+        View view = inflater.inflate(R.layout.chart_layout, container, false);
+
+        LineChart lineChart = (LineChart)view.findViewById(R.id.linechart);
+        List<Entry> entries = new ArrayList<>();
+        entries.add(new Entry(1,6));
+        entries.add(new Entry(2,7));
+        entries.add(new Entry(3,8));
+
+        LineDataSet set = new LineDataSet(entries,"Days/Hours");
+        set.setColor(5);
+        set.getValueTextColor(20);
+
+        LineData lineData = new LineData(set);
+        lineChart.setData(lineData);
+        lineChart.invalidate();
+
+        return view;
     }
 }
 
