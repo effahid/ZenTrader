08a82437ee0f89471978728c273e7a9112998726
Hasham <hasham.rashid@outlook.com>
2016-12-07 06:35:30 +0000

    updated moving chart from example

--- app/src/main/java/com/zentrader/ChartFragment.java
+++ app/src/main/java/com/zentrader/ChartFragment.java
@@ -1,6 +1,8 @@
 package com.zentrader;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.DashPathEffect;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.Fragment;
@@ -14,10 +16,13 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
+import com.github.mikephil.charting.animation.Easing;
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,6 +32,8 @@ public class ChartFragment extends Fragment {
 
     final Handler handler = new Handler();
     protected View mView;
+    private LineChart mChart;
+    List<Entry> entries = new ArrayList<>();
     int counter;
     Random random = new Random();
     public ChartFragment() {
@@ -46,38 +53,80 @@ public class ChartFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.chart_layout, container, false);
         mView= view;
-        Runnable runnable = new Runnable() {
-            @Override
-            public void run() {
-                updateChart();
-                handler.postDelayed(this, 1000);
-            }
-        };
-
-        handler.postDelayed(runnable, 1000);
-
+//        Runnable runnable = new Runnable() {
+//            @Override
+//            public void run() {
+//                updateChart();
+//                handler.postDelayed(this, 2000);
+//            }
+//        };
+//
+//        handler.postDelayed(runnable, 1000);
+        mChart=(LineChart)mView.findViewById(R.id.linechart);
+        setData(45, 100);
         return view;
     }
 
-    private void updateChart() {
-        LineChart lineChart = (LineChart)mView.findViewById(R.id.linechart);
-        List<Entry> entries = new ArrayList<>();
+    private void setData(int count, float range) {
+        ArrayList<Entry> values = new ArrayList<Entry>();
 
+        for (int i = 0; i < count; i++) {
 
+            float val = (float) (Math.random() * range) + 3;
+            values.add(new Entry(i, val));
+        }
+        LineDataSet set1;
 
-        int xValue= Math.round(random.nextInt(10));
-        int yValue= Math.round(random.nextInt(10));
-        entries.add(new Entry(xValue,yValue));
-        counter++;
+        if (mChart.getData() != null &&
+                mChart.getData().getDataSetCount() > 0) {
+            set1 = (LineDataSet) mChart.getData().getDataSetByIndex(0);
+            set1.setValues(values);
+            mChart.getData().notifyDataChanged();
+            mChart.notifyDataSetChanged();
+        } else {
+            // create a dataset and give it a type
+            set1 = new LineDataSet(values, "DataSet 1");
 
-        LineDataSet set = new LineDataSet(entries,"Days/Hours");
-        set.setColor(5);
-        set.getValueTextColor(20);
+            // set the line to be drawn like this "- - - - - -"
+            //set1.enableDashedLine(10f, 5f, 0f);
+            //set1.enableDashedHighlightLine(10f, 5f, 0f);
+            set1.setColor(Color.BLACK);
+            set1.setCircleColor(Color.BLACK);
+            set1.setLineWidth(1f);
+            set1.setCircleRadius(3f);
+            set1.setDrawCircleHole(false);
+            set1.setValueTextSize(9f);
+            set1.setDrawFilled(false);
+            set1.setFormLineWidth(1f);
 
-        LineData lineData = new LineData(set);
-        lineChart.setData(lineData);
+            set1.setFormLineDashEffect(new DashPathEffect(new float[]{10f, 5f}, 0f));
+            set1.setFormSize(15.f);
+            set1.setMode(LineDataSet.Mode.HORIZONTAL_BEZIER);
+//            if (Utils.getSDKInt() >= 18) {
+//                // fill drawable only supported on api level 18 and above
+//                Drawable drawable = ContextCompat.getDrawable(this, R.drawable.fade_red);
+//                set1.setFillDrawable(drawable);
+//            }
+//            else {
+//                set1.setFillColor(Color.BLACK);
+//            }
 
-        lineChart.invalidate();
+            ArrayList<ILineDataSet> dataSets = new ArrayList<ILineDataSet>();
+            dataSets.add(set1); // add the datasets
+
+            // create a data object with the datasets
+            LineData data = new LineData(dataSets);
+
+            // set data
+            mChart.setData(data);
+            //LOOK HERE, COMMENT OUT TO RESTRICT VIEW ..... HELLO
+            //mChart.setVisibleXRange(20,20);
+
+//        mChart.setVisibleYRange(20f, AxisDependency.LEFT);
+//        mChart.centerViewTo(20, 50, AxisDependency.LEFT);
+
+            mChart.animateX(8000, Easing.EasingOption.EaseInCirc);
+        }
     }
 }
 
