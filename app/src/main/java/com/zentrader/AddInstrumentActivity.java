26260e95989255e778658d87a19245a8494266f2
Hasham <hasham.rashid@outlook.com>
2017-03-19 14:19:46 +0000

    added highlight to newly added instruments

--- app/src/main/java/com/zentrader/AddInstrumentActivity.java
+++ app/src/main/java/com/zentrader/AddInstrumentActivity.java
@@ -1,8 +1,13 @@
 package com.zentrader;
 
 import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -11,15 +16,24 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Random;
 
 public class AddInstrumentActivity extends AppCompatActivity {
+
+    List<String> addedInstruments= new ArrayList<String>();
+    Drawable defaultBackground;
+    private int defaultColor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_instrument);
+
         Toolbar instrumentToolbar = (Toolbar) findViewById(R.id.action_addInstrument);
         String [] availableStockName= getAvailableStockName();
         AvailableInstrumentsAdapter adapter = new AvailableInstrumentsAdapter(this,availableStockName);
@@ -103,6 +117,28 @@ public class AddInstrumentActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.add_instrument_menu,menu);
         return true;
+    }
+
+    public void ToggleSelectedInstrument(View view) {
+        TextView selectedTextView = (TextView)view;
+        if(defaultBackground==null){
+            defaultBackground=view.getBackground();
+           //
+        }
+        String stock = selectedTextView.getText().toString();
+
+        if(addedInstruments.contains(stock)){
+            addedInstruments.remove(stock);
+            selectedTextView.setBackground(null);
+            selectedTextView.setTextColor(getResources().getColor(R.color.black));
+        }
+        else{
+            addedInstruments.add(stock);
+            selectedTextView.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
+            selectedTextView.setTextColor(Color.WHITE);
+        }
+
+
     }
 }
 
