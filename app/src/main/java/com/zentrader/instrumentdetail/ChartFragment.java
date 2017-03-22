459d42b86c803f35acaeb055a68121f2a89ffb5f
Hasham <hasham.rashid@outlook.com>
2017-03-22 18:41:13 +0000

    updated chart, made it dynamic

--- app/src/main/java/com/zentrader/instrumentdetail/ChartFragment.java
+++ app/src/main/java/com/zentrader/instrumentdetail/ChartFragment.java
@@ -1,24 +1,15 @@
 package com.zentrader.instrumentdetail;
 
-import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.DashPathEffect;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ListView;
 
-import com.github.mikephil.charting.animation.Easing;
 import com.github.mikephil.charting.charts.LineChart;
-import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
@@ -32,22 +23,53 @@ import java.util.Random;
 
 public class ChartFragment extends Fragment {
 
-    final Handler handler = new Handler();
+
     protected View mView;
     private LineChart mChart;
     List<Entry> entries = new ArrayList<>();
     int counter;
-    Random random = new Random();
+    int xCounter;
+    LineDataSet lineDataSet;
+    Runnable runnable;
+    final Handler handler = new Handler();
+    ArrayList<Entry> values;
+    Random random;
+
+    ArrayList<ILineDataSet> dataSets;
     public ChartFragment() {
         // Required empty public constructor
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-
-
         super.onCreate(savedInstanceState);
         counter++;
+        xCounter=0;
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                UpdateChart();
+
+            }
+        };
+
+    }
+
+    private void UpdateChart() {
+
+        values.add(new Entry(xCounter,xCounter+1));
+
+        LineDataSet set1 = (LineDataSet) mChart.getData().getDataSetByIndex(0);
+        set1.setValues(values);
+        mChart.getData().notifyDataChanged();
+        mChart.notifyDataSetChanged();
+
+        if(xCounter>=9) {
+            xCounter=0;
+        }
+        xCounter++;
+        handler.postDelayed(runnable, 500);
+        //mChart.moveViewToAnimated(10,5, YAxis.AxisDependency.LEFT,1000);
     }
 
     @Override
@@ -55,94 +77,44 @@ public class ChartFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.chart_layout, container, false);
         mView= view;
-//        Runnable runnable = new Runnable() {
-//            @Override
-//            public void run() {
-//                updateChart();
-//                handler.postDelayed(this, 2000);
-//            }
-//        };
-//
-//        handler.postDelayed(runnable, 1000);
         mChart=(LineChart)mView.findViewById(R.id.linechart);
-
-        setData(30, 100);
+        InitializeChart();
+        UpdateChart();
         return view;
     }
 
-    private void setData(int count, float range) {
-        ArrayList<Entry> values = new ArrayList<Entry>();
+    private void InitializeChart() {
+        dataSets = new ArrayList<>();
+        values = new ArrayList<>();
+        values.add(new Entry(1,1));
+        lineDataSet = new LineDataSet(values, "Stock Price");
+        SetupChartDataSetStyle(mChart, lineDataSet);
+        dataSets.add(lineDataSet);
+        LineData data = new LineData(dataSets);
+        mChart.setData(data);
+    }
 
-        for (int i = 0; i < count; i++) {
+    private void SetupChartDataSetStyle(LineChart mChart, LineDataSet lineDataSet) {
+        lineDataSet.setColor(Color.GRAY);
+        lineDataSet.setCircleColor(Color.BLACK);
+        lineDataSet.setLineWidth(1f);
+        lineDataSet.setCircleRadius(3f);
+        lineDataSet.setDrawCircleHole(false);
+        lineDataSet.setValueTextSize(9f);
+        lineDataSet.setDrawFilled(true);
+        lineDataSet.setFormLineWidth(1f);
+        YAxis leftAxis = mChart.getAxisLeft();
+        leftAxis.setEnabled(false);
+        leftAxis.setAxisMaximum(10);
+        leftAxis.setAxisMinimum(1);
 
-            float val = (float) (1200+Math.random() * range) + 3;
-            values.add(new Entry(i, val));
-        }
-        LineDataSet set1;
+        YAxis rightAxis = mChart.getAxisRight();
+        //rightAxis.setAxisMaximum(10);
+        rightAxis.setAxisMinimum(10);
 
-        if (mChart.getData() != null &&
-                mChart.getData().getDataSetCount() > 0) {
-            set1 = (LineDataSet) mChart.getData().getDataSetByIndex(0);
-            set1.setValues(values);
-            mChart.getData().notifyDataChanged();
-            mChart.notifyDataSetChanged();
-        } else {
-            // create a dataset and give it a type
-            set1 = new LineDataSet(values, "DataSet 1");
-
-            // set the line to be drawn like this "- - - - - -"
-            //set1.enableDashedLine(10f, 5f, 0f);
-            //set1.enableDashedHighlightLine(10f, 5f, 0f);
-            set1.setColor(Color.GRAY);
-            set1.setCircleColor(Color.BLACK);
-            set1.setLineWidth(1f);
-            set1.setCircleRadius(3f);
-            set1.setDrawCircleHole(false);
-            set1.setValueTextSize(9f);
-            set1.setDrawFilled(true);
-            set1.setFormLineWidth(1f);
-
-            set1.setFormLineDashEffect(new DashPathEffect(new float[]{10f, 5f}, 0f));
-           // set1.setFormSize(15.f);
-            set1.setMode(LineDataSet.Mode.CUBIC_BEZIER);
-//            if (Utils.getSDKInt() >= 18) {
-//                // fill drawable only supported on api level 18 and above
-//                Drawable drawable = ContextCompat.getDrawable(this, R.drawable.fade_red);
-//                set1.setFillDrawable(drawable);
-//            }
-//            else {
-//                set1.setFillColor(Color.BLACK);
-//            }
-
-            ArrayList<ILineDataSet> dataSets = new ArrayList<ILineDataSet>();
-            dataSets.add(set1); // add the datasets
-
-            // create a data object with the datasets
-            LineData data = new LineData(dataSets);
-
-            // set data
-
-            mChart.setData(data);
-            mChart.setVisibleXRangeMaximum(10);
-
-            mChart.moveViewToAnimated(100,100, YAxis.AxisDependency.LEFT,23000);
+       // lineDataSet.setFormLineDashEffect(new DashPathEffect(new float[]{10f, 5f}, 0f));
+        //lineDataSet.setMode(LineDataSet.Mode.CUBIC_BEZIER);
 
 
-
-
-
-            YAxis leftAxis = mChart.getAxisLeft();
-
-            leftAxis.setEnabled(false);
-
-
-
-
-
-
-        }
     }
 }
-
-
-
